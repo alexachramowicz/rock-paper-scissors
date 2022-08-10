a65e8f1544a66321a57850b10627cb633f82eccf
alex <alex.achramowicz@gmail.com>
2022-08-09 23:38:01 -0700

    added game functionality

--- script.js
+++ script.js
@@ -8,24 +8,31 @@ function playRound(playerSelection, computerSelection) {
     console.log(playerSelection + " vs " + computerSelection);
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            return "You Lose! Rock loses to paper!";
+            console.log("You Lose! Rock loses to paper!");
+            return 0;
         } else if (computerSelection === "scissors") {
-            return "You Win! Rock beats scissors!";
+            console.log("You Win! Rock beats scissors!");
+            return 1;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
-            return "You Lose! Paper loses to scissors!";
+            console.log("You Lose! Paper loses to scissors!");
+            return 0;
         } else if (computerSelection === "rock") {
-            return "You Win! Paper beats rock!";
+            console.log("You Win! Paper beats rock!");
+            return 1;
         }
     } else {    // playerSelection === "scissors"
         if (computerSelection === "rock") {
-            return "You Lose! Scissors loses to rock!";
+            console.log("You Lose! Scissors loses to rock!");
+            return 0;
         } else if (computerSelection === "paper") {
-            return "You Win! Scissors beats paper!";
+            console.log("You Win! Scissors beats paper!");
+            return 1;
         }
     }
-    return `Push! ${playerSelection} ties to ${computerSelection}`;
+    console.log(`Push! ${playerSelection} ties to ${computerSelection}`);
+    return 2;
 }
 
 function getPlayerSelection() {
@@ -41,10 +48,22 @@ function getPlayerSelection() {
 function game() {
     let playerWins = 0;
     let computerWins = 0;
+    let winner = 0;
     for(let i = 0; i < 5; i++) {
-        playRound(getPlayerSelection(), getComputerChoice());
+        winner = playRound(getPlayerSelection(), getComputerChoice());
+        if(winner === 1)
+            ++playerWins;
+        else if (winner === 0)
+            ++computerWins;
+        else
+            continue;
     }
-    
+    if (playerWins > computerWins) 
+        return `Player wins ${playerWins} to ${computerWins}`;
+    else if (computerWins > playerWins)
+        return  `Computer wins ${computerWins} to ${playerWins}`;
+    else
+        return `It's a tie! ${playerWins} to ${computerWins}`
 }
 
-game();
+console.log(game());
