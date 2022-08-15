bb3a557589ef44a0cb58dde518851da7b40a4a55
alexander <alex.achramowicz@gmail.com>
2022-08-15 15:05:53 -0700

    updated game script

--- script.js
+++ script.js
@@ -6,68 +6,89 @@ function getComputerSelection() {
 
 function playRound(playerSelection, computerSelection) {
     const winnerPara = document.querySelector('#winner-para');
-    console.log(playerSelection + " vs " + computerSelection);
+    
+    winnerPara.textContent = playerSelection + " vs " + computerSelection;
 
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
             winnerPara.textContent = "You Lose! Rock loses to paper!";
-            console.log("You Lose! Rock loses to paper!");
-            return 0;
+            return COMPUTER;
         } else if (computerSelection === "scissors") {
             winnerPara.textContent = "You Win! Rock beats scissors!";
-            console.log("You Win! Rock beats scissors!");
-            return 1;
+            return PLAYER;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
             winnerPara.textContent = "You Lose! Paper loses to scissors!";
-            console.log("You Lose! Paper loses to scissors!");
-            return 0;
+            return COMPUTER;
         } else if (computerSelection === "rock") {
             winnerPara.textContent = "You Win! Paper beats rock!";
-            console.log("You Win! Paper beats rock!");
-            return 1;
+            return PLAYER;
         }
     } else {    // playerSelection === "scissors"
         if (computerSelection === "rock") {
             winnerPara.textContent = "You Lose! Scissors loses to rock!";
-            console.log("You Lose! Scissors loses to rock!");
-            return 0;
+            return COMPUTER;
         } else if (computerSelection === "paper") {
             winnerPara.textContent = "You win! Scissors beats paper!";
-            console.log("You Win! Scissors beats paper!");
-            return 1;
+            return PLAYER;
         }
     }
-    console.log(`Push! ${playerSelection} ties to ${computerSelection}`);
-    return 2;
+    winnerPara.textContent = `Push! ${playerSelection} ties to ${computerSelection}`;
+    return TIE;
 }
 
-// function calcScore(result) {
-//     if(result === 1) {
-//         playerCount++;
-//     } else if(result === 0) {
-//         computerCount++;
-//     } else {
-//         //tie?
-//         ;
-//     }
-// }
+function updateScore(winner) {
+    if(winner === PLAYER) {
+        playerCount = parseInt(playerCounter.textContent);
+        playerCounter.textContent = ++playerCount;
+    } else if(winner === COMPUTER) {
+        computerCount = parseInt(computerCounter.textContent);
+        computerCounter.textContent = ++computerCount;
+    } else { // tie
+        ;
+    }
+
+    calcWinner();
+    return;
+}
+
+function calcWinner() {
+
+    if(parseInt(playerCounter.textContent) === 5) {
+        winner.textContent = "Winner"
+        playerCounter.textContent = 0;
+        computerCounter.textContent = 0;
+    } else if(parseInt(computerCounter.textContent) === 5) {
+        winner.textContent = "Defeat"
+        playerCounter.textContent = 0;
+        computerCounter.textContent = 0;
+    }
+    return;
+}
 
 // decls n defns
-const btns = document.querySelectorAll('.btn');
+const COMPUTER = 0;
+const PLAYER = 1
+const TIE = 2;
+
+let result;
 let playerSelection;
 let computerSelection;
+let playerCount;
+let computerCount;
+
+const btns = document.querySelectorAll('.btn');
 const playerCounter = document.querySelector('#player-counter');
 const computerCounter = document.querySelector('#computer-counter');
-let playerCount = 0;
-let computerCount = 0;
+const winnerSpan = document.querySelector('#winner');
 
 // driver
 btns.forEach((btn) => {
     btn.addEventListener('click', () => {
         playerSelection = btn.textContent.toLowerCase();
         computerSelection = getComputerSelection();
-        let result = playRound(playerSelection, computerSelection);
+        result = playRound(playerSelection, computerSelection);
+        updateScore(result);
     })
 });
