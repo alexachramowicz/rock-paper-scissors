b5255d7ee956020f3049a426c36cae33eff04826
alex <alex.achramowicz@gmail.com>
2022-08-15 17:04:24 -0700

    added helper functions

--- script.js
+++ script.js
@@ -5,36 +5,35 @@ function getComputerSelection() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    const winnerPara = document.querySelector('#winner-para');
     
-    winnerPara.textContent = playerSelection + " vs " + computerSelection;
+    resultPara.textContent = playerSelection + " vs " + computerSelection;
 
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            winnerPara.textContent = "You Lose! Rock loses to paper!";
+            resultPara.textContent = "You Lose! Rock loses to paper!";
             return COMPUTER;
         } else if (computerSelection === "scissors") {
-            winnerPara.textContent = "You Win! Rock beats scissors!";
+            resultPara.textContent = "You Win! Rock beats scissors!";
             return PLAYER;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
-            winnerPara.textContent = "You Lose! Paper loses to scissors!";
+            resultPara.textContent = "You Lose! Paper loses to scissors!";
             return COMPUTER;
         } else if (computerSelection === "rock") {
-            winnerPara.textContent = "You Win! Paper beats rock!";
+            resultPara.textContent = "You Win! Paper beats rock!";
             return PLAYER;
         }
     } else {    // playerSelection === "scissors"
         if (computerSelection === "rock") {
-            winnerPara.textContent = "You Lose! Scissors loses to rock!";
+            resultPara.textContent = "You Lose! Scissors loses to rock!";
             return COMPUTER;
         } else if (computerSelection === "paper") {
-            winnerPara.textContent = "You win! Scissors beats paper!";
+            resultPara.textContent = "You win! Scissors beats paper!";
             return PLAYER;
         }
     }
-    winnerPara.textContent = `Push! ${playerSelection} ties to ${computerSelection}`;
+    resultPara.textContent = `Push! ${playerSelection} ties to ${computerSelection}`;
     return TIE;
 }
 
@@ -42,37 +41,41 @@ function updateScore(winner) {
     if(winner === PLAYER) {
         playerCount = parseInt(playerCounter.textContent);
         playerCounter.textContent = ++playerCount;
+        playerCounter.setAttribute = ("style", "color: green");
     } else if(winner === COMPUTER) {
         computerCount = parseInt(computerCounter.textContent);
         computerCounter.textContent = ++computerCount;
     } else { // tie
         ;
     }
-
-    calcWinner();
     return;
 }
 
 function calcWinner() {
-
-    if(parseInt(playerCounter.textContent) === 5) {
-        winner.textContent = "Winner"
-        playerCounter.textContent = 0;
-        computerCounter.textContent = 0;
-    } else if(parseInt(computerCounter.textContent) === 5) {
-        winner.textContent = "Defeat"
-        playerCounter.textContent = 0;
-        computerCounter.textContent = 0;
+    if(parseInt(playerCounter.textContent) === BESTOF) {
+        winnerSpan.textContent = "Winner"
+        resetScore();
+        return true;
+    } else if(parseInt(computerCounter.textContent) === BESTOF) {
+        winnerSpan.textContent = "Defeat"
+        resetScore();
+        return true;
     }
-    return;
+    return false;
+}
+
+function resetScore() {
+    playerCounter.textContent = 0;
+    computerCounter.textContent = 0;
 }
 
 // decls n defns
 const COMPUTER = 0;
 const PLAYER = 1
 const TIE = 2;
+const BESTOF = 5;
 
-let result;
+let winner;
 let playerSelection;
 let computerSelection;
 let playerCount;
@@ -81,6 +84,7 @@ let computerCount;
 const btns = document.querySelectorAll('.btn');
 const playerCounter = document.querySelector('#player-counter');
 const computerCounter = document.querySelector('#computer-counter');
+const resultPara = document.querySelector('#result-para');
 const winnerSpan = document.querySelector('#winner');
 
 // driver
@@ -88,7 +92,8 @@ btns.forEach((btn) => {
     btn.addEventListener('click', () => {
         playerSelection = btn.textContent.toLowerCase();
         computerSelection = getComputerSelection();
-        result = playRound(playerSelection, computerSelection);
-        updateScore(result);
+        winner = playRound(playerSelection, computerSelection);
+        updateScore(winner);
+        calcWinner();
     })
 });
